fa8580560c43676fdafe4c466ec61609bdb0c1ac
blackdragon101 <153359629+blackdragon101@users.noreply.github.com>
2025-05-31 05:29:33 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Contact Form Alert (Demo purpose)
-document.addEventListener("DOMContentLoaded", function() {
-  const form = document.getElementById('contact-form');
-  form.addEventListener('submit', function(e){
-    e.preventDefault();
-    alert('Message sent! (Demo only)');
-    form.reset();
-  });
-});
